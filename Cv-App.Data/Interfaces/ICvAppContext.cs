b533571fb23ce92bddf6b39f8d45325da2af3e80
Modris-lat <belkovskis.modris@gmail.com>
2021-02-26 16:58:32 +0200

    dbContext

---
+++ Cv-App.Data/Interfaces/ICvAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Cv_App.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Cv_App.Data.Interfaces
+{
+    public interface ICvAppContext
+    {
+        DbSet<T> Set<T>() where T : class;
+        EntityEntry<T> Entry<T>(T entity) where T : class;
+        int SaveChanges();
+        Task<int> SaveAsync();
+        Task EnsureDataBaseCreated();
+        DbSet<CvModel> CvModels { get; set; }
+    }
+}
---
+++ Cv-App.Data/Models/CvAppContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Cv_App.Core.Models;
+using Cv_App.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cv_App.Data.Models
+{
+    public class CvAppContext: DbContext, ICvAppContext
+    {
+        public CvAppContext(DbContextOptions<CvAppContext> options):
+            base(options){}
+
+        public DbSet<CvModel> CvModels { get; set; }
+
+        public async Task<int> SaveAsync()
+        {
+            return await SaveChangesAsync();
+        }
+
+        public async Task EnsureDataBaseCreated()
+        {
+            await Database.EnsureCreatedAsync();
+        }
+    }
+}
