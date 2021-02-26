3369e3ff62396d9eaf1214bfc442c21ce4f2ecdc
Modris-lat <belkovskis.modris@gmail.com>
2021-02-26 22:41:00 +0200

    API Service

---
+++ Cv-App.ApiService/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cv_App.Data.Interfaces;
+
+namespace Cv_App.ApiService
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ICvAppContext dataContext)
+        {
+            dataContext.EnsureDataBaseCreated();
+        }
+    }
+}
