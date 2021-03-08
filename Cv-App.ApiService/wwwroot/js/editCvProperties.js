f40465736525404e643bdc5cb9cbdec0ddd31f0a
Modris-lat <belkovskis.modris@gmail.com>
2021-03-08 12:57:02 +0200

    edit view can add new input items

--- Cv-App.ApiService/wwwroot/js/editCvProperties.js
+++ Cv-App.ApiService/wwwroot/js/editCvProperties.js
@@ -1,65 +1,61 @@
 ﻿$(document).ready(function () {
-    var educationCount = 0;
     $("#addEducation").bind("click", function () {
-        var index = educationCount;
-        $("#education").append("<div class=\"form-group\">" +
+        var index = $(".additionalEducation").length;
+        $("#education").append("<span class=\"additionalEducation\">###########</span>" +
+            "<div class=\"form-group\">" +
             "<label>Degree</label>" +
-            "<input type=\"text\" name=\"Educations[Model.Educations.Count + " + index + "].Degree\" class=\"form-control\" id=\"" + index + "\"/>" +
+            "<input type=\"text\" name=\"Educations[" + index + "].Degree\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>Specialization</label>" +
-            "<input type=\"text\" name=\"Educations[Model.Educations.Count + " + index + "].Specialization\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"Educations[" + index + "].Specialization\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>School</label>" +
-            "<input type=\"text\" name=\"Educations[Model.Educations.Count + " + index + "].School\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"Educations[" + index + "].School\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>Start Date</label>" +
-            "<input type=\"text\" name=\"Educations[Model.Educations.Count + " + index + "].StartDate\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"Educations[" + index + "].StartDate\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>End Date</label>" +
-            "<input type=\"text\" name=\"Educations[Model.Educations.Count + " + index + "].EndDate\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"Educations[" + index + "].EndDate\" class=\"form-control\" //>" +
             "</div>");
-        educationCount = educationCount + 1;
     });
-    var workCount = 0;
     $("#addWorkExperience").bind("click", function () {
-        var index = workCount;
-        $("#work").append("<div class=\"form-group\">" +
+        var index = $(".additionalWorkExperience").length;
+        $("#work").append("<span class=\"additionalWorkExperience\">###########</span>" + "<div class=\"form-group\">" +
             "<label>Job Title</label>" +
-            "<input type=\"text\" name=\"WorkExperiences[Model.WorkExperiences.Count + " + index + "].JobTitle\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"WorkExperiences[" + index + "].JobTitle\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>Company</label>" +
-            "<input type=\"text\" name=\"WorkExperiences[Model.WorkExperiences.Count + " + index + "].Company\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"WorkExperiences[" + index + "].Company\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>Start Date</label>" +
-            "<input type=\"text\" name=\"WorkExperiences[Model.WorkExperiences.Count + " + index + "].StartDate\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"WorkExperiences[" + index + "].StartDate\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>End Date</label>" +
-            "<input type=\"text\" name=\"WorkExperiences[Model.WorkExperiences.Count + " + index + "].EndDate\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"WorkExperiences[" + index + "].EndDate\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>Job Description</label>" +
-            "<input type=\"text\" name=\"WorkExperiences[Model.WorkExperiences.Count + " + index + "].JobDescription\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"WorkExperiences[" + index + "].JobDescription\" class=\"form-control\" //>" +
             "</div>");
-        workCount = workCount + 1;
     });
-    var otherCount = 0;
-    $("#addAdditionalInformation").bind("click", function (e) {
-        var index = otherCount;
-        $("#other").append("<div class=\"form-group\">" +
+    $("#addAdditionalInformation").bind("click", function () {
+        var index = $(".additionalInfo").length;
+        $("#other").append("<span class='additionalInfo'>###########</span>" +
+            "<div class=\"form-group\"" +
             "<label>Title</label>" +
-            "<input type=\"text\" name=\"Properties[" + index + "].Title\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"Properties[" + index + "].Title\" class=\"form-control\" //>" +
             "</div>" +
             "<div class=\"form-group\">" +
             "<label>Text Field</label>" +
-            "<input type=\"text\" name=\"Properties[" + index + "].Text\" class=\"form-control\" id=\"" + index + "\"//>" +
+            "<input type=\"text\" name=\"Properties[" + index + "].Text\" class=\"form-control\" //>" +
             "</div>");
-        otherCount = otherCount + 1;
     });
 });
